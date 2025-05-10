627cf3dbfca7c7a50aa0477d4217fcff13e5937d
nohanmohsin <nohanmohsin@gmail.com>
2025-05-10 22:37:30 +0600

    system instructions and structured output for disease detection

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { createUserContent, createPartFromUri } from "@google/genai";
+import { createUserContent, createPartFromUri, Type } from "@google/genai";
 import { ai } from "../util/aiFunctions";
-
 const ImageUploader = () => {
   const [image, setImage] = useState(null);
   // const [status, setStatus] = useState("idle");
@@ -12,6 +11,7 @@ const ImageUploader = () => {
       console.log(e.target.files[0]);
     }
   }
+
   async function uploadImage() {
     if (image) {
       const imgBlob = await fetch(URL.createObjectURL(image)).then((r) =>
@@ -19,7 +19,7 @@ const ImageUploader = () => {
       );
       const imgFile = await ai.files.upload({
         file: imgBlob,
-        config: { mimeType: "image/png" },
+        config: { mimeType: image.type },
       });
       console.log(imgFile.uri);
       const response = await ai.models.generateContent({
@@ -28,10 +28,46 @@ const ImageUploader = () => {
           createPartFromUri(imgFile.uri, imgFile.mimeType),
           "",
         ]),
+        config: {
+          systemInstruction:
+            "only generate cures and remedies for ill plants. Return a false in diseaseDetected if you see any image that doesn't resemble a plant or the plant seems to be healthy. Try your best to generate the text in Bangla. diseaseRemedyAudioScript will also be in Bangla but with English letters keep the bangla wording very casual and phonetically sound them out in the audio script. make sure the script for audio version of the disease remedy is the same size as the diseaseRemedy",
+          responseMimeType: "application/json",
+          responseSchema: {
+            type: Type.OBJECT,
+            properties: {
+              treeName: {
+                type: Type.STRING,
+              },
+              diseaseDetected: {
+                type: Type.BOOLEAN,
+              },
+              treeDisease: {
+                type: Type.ARRAY,
+                items: {
+                  type: Type.STRING,
+                },
+              },
+              diseaseRemedy: {
+                type: Type.STRING,
+              },
+              diseaseRemedyAudioScript: {
+                type: Type.STRING,
+              },
+            },
+            propertyOrdering: [
+              "treeName",
+              "treeDisease",
+              "diseaseDetected",
+              "diseaseRemedy",
+              "diseaseRemedyAudioScript",
+            ],
+          },
+        },
       });
       console.log(response.text);
     }
   }
+
   return (
     <div>
       <label htmlFor="imageuploader">upload an image</label>
--- src/util/aiFunctions.js
+++ src/util/aiFunctions.js
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI, Type } from "@google/genai";
 
 export const ai = new GoogleGenAI({
   apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
