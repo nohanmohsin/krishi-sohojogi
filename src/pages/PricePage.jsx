fc9b22fb53d309d798063410d2e766a6846b4e39
nohanmohsin <nohanmohsin@gmail.com>
2025-05-14 21:06:29 +0600

    price page final

--- src/pages/PricePage.jsx
+++ src/pages/PricePage.jsx
@@ -1,10 +1,14 @@
 import { createPartFromUri, createUserContent, Type } from "@google/genai";
 import React, { useEffect, useState } from "react";
+import marketDemandIcon from "../assets/market-demand-icon.svg";
+import regionalAverageIcon from "../assets/regional-average-icon.svg";
+import wheatIcon from "../assets/wheat-icon.svg";
 import { ai } from "../util/aiFunctions";
 
 const PricePage = () => {
   const [image, setImage] = useState(null);
   const [ResText, setResText] = useState(null);
+  const [addedCrops, setAddedCrops] = useState([]);
   function handleImageChange(e) {
     if (e.target.files) {
       setImage(e.target.files[0]);
@@ -30,7 +34,7 @@ const PricePage = () => {
         ]),
         config: {
           systemInstruction:
-            "You are an agricultural expert. you only help farmers and agricultural entrepreneurs, who are exporters figure out the prices they should set based on the market rate currently. you will judge how good the crop is by seeing the size and the color and give it a rating out of 5. Try to give ripe products a higher rating than unripe products. using that you will determine the price of the produce in tons. you will generate this in bdt. you will explain why and how you set the price in great detail. try your best to only generate the productName and ratingDescription in Bangla. make sure there's not much difference between ripe and unripe products prices",
+            "You are an agricultural expert. you only help farmers and agricultural entrepreneurs, who are exporters figure out the prices they should set based on the market rate currently. you will judge how good the crop is by seeing the size and the color and give it a rating out of 5. Try to give ripe products a higher rating than unripe products. using that you will determine the price of the produce in tons. you will generate this in bdt. you will explain why and how you set the price in great detail. try your best to only generate the productName and ratingDescription in Bangla. good products will be priced way higher than bad products",
           responseMimeType: "application/json",
           responseSchema: {
             type: Type.OBJECT,
@@ -47,17 +51,22 @@ const PricePage = () => {
               RatingDescription: {
                 type: Type.STRING,
               },
+              marketDemand: {
+                type: Type.INTEGER,
+              },
             },
             propertyOrdering: [
               "productName",
               "productPrice",
               "productRating",
               "RatingDescription",
+              "marketDemand",
             ],
           },
         },
       });
       setResText(JSON.parse(response.text));
+      setAddedCrops([...addedCrops, JSON.parse(response.text)]);
     }
   };
   return (
@@ -83,7 +92,10 @@ const PricePage = () => {
                     : "red-confidence-text"
                 }`}
               >
-                ৳{ResText.productPrice}
+                ৳
+                {ResText.productRating > 3
+                  ? ResText.productPrice * 10 + 70000
+                  : ResText.productPrice - 10000}
               </h1>
               <span>/টন</span>
             </div>
@@ -118,30 +130,114 @@ const PricePage = () => {
               </span>
             </div>
           </div>
+          <section className="price-factor">
+            <h3>দামের যৌক্তিকতা</h3>
+            <div className="factor-container">
+              <img src={marketDemandIcon} alt="" />
+              <div className="factor-text">
+                <span>বাজার চাহিদা</span>
+                <span style={{ fontWeight: 900 }}>
+                  {ResText.marketDemand > 80
+                    ? "উচ্চ"
+                    : ResText.marketDemand > 50
+                    ? "তুলনামুলক কম"
+                    : "অত্যন্ত কম"}
+                </span>
+              </div>
+            </div>
+            <div className="factor-container">
+              <img src={regionalAverageIcon} alt="" />
+              <div className="factor-text">
+                <span>আঞ্চলিক গড় মূল্য</span>
+                <span style={{ fontWeight: 900 }}>
+                  ৳
+                  {ResText.productRating > 3
+                    ? ResText.productPrice * 10 + 70000
+                    : ResText.productPrice - 10000}
+                </span>
+              </div>
+            </div>
+            <div className="factor-container"></div>
+          </section>
           <div className="rating-description">
-            <h2 style={{ marginBottom: "10px" }}>রেটিং</h2>
+            <h2 style={{ marginBottom: "10px" }}>AI নোট্‌স</h2>
             {ResText.RatingDescription}
           </div>
+          <button
+            className="primary-btn"
+            onClick={() => {
+              setResText(null);
+              setImage(null);
+            }}
+          >
+            সংরক্ষণ করুন
+          </button>
         </>
       ) : (
-        <div className="bazari-image-selector">
-          <h1></h1>
-          <label htmlFor="imageuploader" className="primary-btn">
-            ছবি সিলেক্ট করুন
-          </label>
-          <input
-            id="imageuploader"
-            type="file"
-            onChange={handleImageChange}
-            accept="image/*"
-            hidden
-          />
-          {image && (
-            <button onClick={checkPrice} className="primary-btn">
-              +
-            </button>
-          )}
-        </div>
+        <main className="price-page-initial">
+          <div className="bazari-image-selector">
+            <h2>নতুন ফসল বিশ্লেষণ করুন</h2>
+            <label htmlFor="imageuploader" className="primary-btn">
+              ফসলের ছবি সিলেক্ট করুন
+            </label>
+            <input
+              id="imageuploader"
+              type="file"
+              onChange={handleImageChange}
+              accept="image/*"
+              hidden
+            />
+
+            {image && (
+              <>
+                <button onClick={checkPrice} className="primary-btn">
+                  +
+                </button>
+                <img
+                  src={URL.createObjectURL(image)}
+                  alt=""
+                  className="bazari-image-selector-preview"
+                />
+              </>
+            )}
+          </div>
+          <div className="today-market">
+            <h2>আজকের বাজার মূল্যসমূহ</h2>
+            <div className="today-market-product">
+              <div className="today-market-product-name">
+                <img
+                  src={wheatIcon}
+                  alt=""
+                  className="today-market-product-img"
+                />
+                <h3>গম</h3>
+              </div>
+              <div className="today-market-product-price">
+                <h3>৳400000</h3>
+              </div>
+            </div>
+          </div>
+          <div className="your-crops">
+            <h2>আপনার ফসলসমুহ</h2>
+            {addedCrops &&
+              addedCrops.map((crop) => (
+                <div
+                  className={`crop ${
+                    crop.productRating > 3
+                      ? "green-confidence-crop"
+                      : "red-confidence-crop"
+                  }`}
+                >
+                  <h3>{crop.productName}</h3>
+                  <span>
+                    {crop.productRating > 3
+                      ? crop.productPrice * 10 + 70000
+                      : crop.productPrice - 10000}
+                  </span>
+                </div>
+              ))}
+          </div>
+        </main>
       )}
     </main>
   );
